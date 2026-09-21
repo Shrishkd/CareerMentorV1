112c4a538301387da9bc3408c32c178aef527a3a
Shrish <shrishdas444@gmail.com>
2026-09-21 22:08:00 +0530

    Fixed minor issue

--- Frontend/src/pages/Index.tsx
+++ Frontend/src/pages/Index.tsx
@@ -95,7 +95,7 @@ const feedbacks = [
       "Personalized technical and HR questions made preparation focused. The feedback report strengthened my communication skills."
   },
   {
-    name: "Aayushi Shrivastav",
+    name: "Aarchi Jha",
     role: "Backend Engineer",
     avatar: "/avatars/Aayushi.jpg",
     text:
